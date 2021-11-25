d19c37018cbb74a77be9ee53e64538f1b5b28078
Sarama3118 <ramissara@gmail.com>
2021-11-25 14:54:40 +0100

    user 1 terminada

--- src/main/java/net/filmcity/app/controllers/MoviesController.java
+++ src/main/java/net/filmcity/app/controllers/MoviesController.java
@@ -3,8 +3,7 @@ package net.filmcity.app.controllers;
 import net.filmcity.app.domain.Movie;
 import net.filmcity.app.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,11 @@ public class MoviesController {
     @GetMapping("/movies")
     public List<Movie> allMovies() {
         return movieRepository.findAll();
+    }
+
+    @PostMapping("/movies")
+    public Movie addMovie(@RequestBody Movie movie) {
+        movieRepository.save(movie);
+        return movie;
     }
 }
